94efdc23cc2b454787964225f9d5d0a852e1e2a2
sid-dias <siddharth@infoworks.io>
2016-06-01 15:33:51 +0530

    changes

--- getArtifacts.py
+++ getArtifacts.py
@@ -9,32 +9,37 @@ import sys
 username = 'admin'
 password = 'benedict'
 
+
 def get_artifact(repo, projName, version_no, fileName):
     url = 'http://172.17.0.1:8081/artifactory/%s/%s/%s/%s' % (repo, projName, version_no, fileName)
     r = requests.get(url, auth=(username, password))
-    if not os.path.exists(projName):
-        os.makedirs(projName)
-    file = open("%s/%s" % (projName, fileName), "wb")
+    if not os.path.exists("artifacts/"+projName):
+        os.makedirs("artifacts/"+projName)
+    file = open("artifacts/%s/%s" % (projName, fileName), "wb")
     file.write(r.content)
+
 
 version = sys.argv[1]
 url = 'http://172.17.0.1:8081/artifactory/versionInfo/%s/' % version
 r = requests.get(url, auth=(username, password))
 
-listFile = str(r.content,encoding="utf-8")
-fileNames = re.findall(r'\n<a href=\"([^"]*)\"',listFile)
+listFile = str(r.content, encoding="utf-8")
+fileNames = re.findall(r'\n<a href=\"([^"]*)\"', listFile)
 fileNames = list(filter(lambda x: not (x.endswith("md5") or x.endswith("sha1")), fileNames))
 print(fileNames)
 
 if not os.path.exists("build-info"):
-        os.makedirs("build-info")
+    os.makedirs("build-info")
 
 for file in fileNames:
-    url = 'http://172.17.0.1:8081/artifactory/versionInfo/%s/%s' %(version,file)
+    url = 'http://172.17.0.1:8081/artifactory/versionInfo/%s/%s' % (version, file)
     r = requests.get(url, auth=(username, password))
     fd = open("build-info/%s" % file, "wb")
     fd.write(r.content)
     fd.close()
+
+if not os.path.exists("artifacts"):
+    os.makedirs("artifacts")
 
 for fileName in glob.glob("./build-info/*.json"):
     fd = open(fileName)
@@ -47,4 +52,4 @@ for fileName in glob.glob("./build-info/*.json"):
     for artifact in jdata["buildInfo"]["modules"][0]["artifacts"]:
         get_artifact(repo, projName, version_no, artifact["name"])
 
-    print("Done fetching artifacts of "+projName)
+    print("Done fetching artifacts of " + projName)
