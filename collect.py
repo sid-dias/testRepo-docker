c29e2c2095334f8436a57b10e5b645325e47ca8c
sid-dias <siddharth@infoworks.io>
2016-06-06 12:17:58 +0530

    ssup

--- collect.py
+++ collect.py
@@ -2,7 +2,7 @@
 
 import requests
 import json
-import os
+import sys
 
 
 def get_json(url):
@@ -12,29 +12,18 @@ def get_json(url):
     return r.json()
 
 
-if not os.path.exists("build-info"):
-    os.makedirs("build-info")
+projName = sys.argv[1]
+data = get_json('http://localhost:8081/artifactory/api/build/%s' % projName)
+maxBuildNo = data['buildsNumbers'][0]['uri'][1:]
 
-mf = open("manifest.txt", "w")
+for obj in data['buildsNumbers']:
+    if obj['uri'][1:] > maxBuildNo:
+        maxBuildNo = obj['uri'][1:]
 
-with open("projectList.txt") as f:
-    projNames = f.readlines()
+data = get_json('http://localhost:8081/artifactory/api/build/%s/%s' % (projName, maxBuildNo))
+version_no = data["buildInfo"]["properties"]["buildInfo.env.VERSION_NUMBER"]
 
-for projName in projNames:
-    projName = projName.strip('\n')
-    data = get_json('http://localhost:8081/artifactory/api/build/%s' % projName)
-    maxBuildNo = data['buildsNumbers'][0]['uri'][1:]
-    for obj in data['buildsNumbers']:
-        if obj['uri'][1:] > maxBuildNo:
-            maxBuildNo = obj['uri'][1:]
+fd = open("%s.json" %version_no, "w")
+json.dump(data, fd)
+fd.close()
 
-    data = get_json('http://localhost:8081/artifactory/api/build/%s/%s' % (projName, maxBuildNo))
-    version_no = data["buildInfo"]["properties"]["buildInfo.env.VERSION_NUMBER"].strip()
-    commit_id = data["buildInfo"]["properties"]["buildInfo.env.GIT_COMMIT"]
-    mf.write("%s:\nVERSION NUMBER: %s\nGIT COMMIT ID: %s\n" % (projName, version_no, commit_id))
-
-    fd = open("build-info/%s_%s.json" % (projName, version_no), "w")
-    json.dump(data, fd)
-    fd.close()
-
-mf.close()
