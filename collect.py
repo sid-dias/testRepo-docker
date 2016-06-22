669230f008d467001e4e0b7e54a5958906eba434
sid-dias <siddharth@infoworks.io>
2016-06-22 07:59:59 +0530

    ssup

--- collect.py
+++ collect.py
@@ -20,6 +20,7 @@ for obj in data['buildsNumbers']:
     if int(obj['uri'][1:]) > int(maxBuildNo):
         maxBuildNo = obj['uri'][1:]
 
+projName = data["buildInfo"]["properties"]["buildInfo.env.PROJECT_NAME"]
 data = get_json('http://localhost:8081/artifactory/api/build/%s/%s' % (projName, maxBuildNo))
 commit_id = data["buildInfo"]["properties"]["buildInfo.env.GIT_COMMIT"]
 version_no = data["buildInfo"]["properties"]["buildInfo.env.VERSION_NUMBER"]
